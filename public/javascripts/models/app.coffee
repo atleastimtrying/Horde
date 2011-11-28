304b03f04fddc368b619f3ff3a9c001adb4895b1
Anders Fisher <atleastimtrying@gmail.com>
2011-11-28 14:19:53 +0000

    backboen added

--- public/javascripts/models/app.coffee
+++ public/javascripts/models/app.coffee
@@ -1,2 +1,14 @@
-$ ->
-  window.game = new window.Game()
+require [
+  "javascripts/models/game.js"
+  "javascripts/models/bullet.js"
+  "javascripts/models/enemy.js"
+  "javascripts/models/input.js"
+  "javascripts/models/player.js"
+  "javascripts/models/sockets.js"
+  "javascripts/models/twittermanager.js"
+  "javascripts/collections/bullets.js"
+  "javascripts/collections/enemies.js"
+  "javascripts/collections/players.js"
+  ], ($)->
+  $ ->
+    window.game = new window.Game()
--- public/javascripts/models/app.js
+++ public/javascripts/models/app.js
@@ -1,4 +1,6 @@
 
-  $(function() {
-    return window.game = new window.Game();
+  require(["javascripts/models/game.js", "javascripts/models/bullet.js", "javascripts/models/enemy.js", "javascripts/models/input.js", "javascripts/models/player.js", "javascripts/models/sockets.js", "javascripts/models/twittermanager.js", "javascripts/collections/bullets.js", "javascripts/collections/enemies.js", "javascripts/collections/players.js"], function($) {
+    return $(function() {
+      return window.game = new window.Game();
+    });
   });
